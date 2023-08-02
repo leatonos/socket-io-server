c640181412817a392e2260fceb70141bd05c44d5
Glitch (glitch-hello-node) <none>
2023-08-02 03:45:05 -0400

    ajusted the PORT for heroku

--- server.js
+++ server.js
@@ -145,6 +145,9 @@ const removeDuckFromRoom = (roomId,duckId) =>{
   }
 }
 
-server.listen(3001, () => {
-  console.log('listening on *:3000');
+const port = process.env.PORT || 3000
+
+
+server.listen(port, () => {
+  console.log(`Server is running on port ${port}`);
 });
